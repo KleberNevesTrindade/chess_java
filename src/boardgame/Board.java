8cf38851e002d767a3b6531a4a11dfeb1026cd92
KleberNevesTrindade <kleber.n.trindade@gmail.com>
2022-03-21 12:47:36 -0300

    BoardException e tratamento de erros

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -7,6 +7,9 @@ public class Board {
 	private Piece[][] pieces;
 	
 	public Board(int rows, int colunms) {
+		if (rows < 1 || colunms <1) {
+			throw new BoardException("Erro ao criar o tabuleiro");
+		}
 		this.rows = rows;
 		this.colunms = colunms;
 		pieces = new Piece[rows][colunms];
@@ -16,29 +19,47 @@ public class Board {
 		return rows;
 	}
 
-	public void setRows(int rows) {
-		this.rows = rows;
-	}
-
 	public int getColunms() {
 		return colunms;
 	}
 
-	public void setColunms(int colunms) {
-		this.colunms = colunms;
-	}
-	
+	// Pega a peća pela linha e coluna
 	public Piece piece(int row, int colunm) {
+		if (!positionExists(row, colunm)) {
+			throw new BoardException("Peća fora do tabuleiro");
+		}
 		return pieces[row][colunm];
 	}
 	
+	// Pega a pela posićão
 	public Piece piece(Position position) {
+		if (!positionExists(position)) {
+			throw new BoardException("Peća fora do tabuleiro");
+		}
 		return pieces[position.getRow()][position.getColunm()];
 	}
 	
 	public void placePiece(Piece piece, Position position) {
+		if (thereIsAPiece(position)) {
+			throw new BoardException("Já existe uma peća nessa posićão? " + position);
+		}
 		pieces[position.getRow()][position.getColunm()] = piece;
 		piece.position = position;
+	}
+	
+	private boolean positionExists(int row, int colunm) {
+		return row >= 0 && row < rows && colunm >= 0 && colunm < colunms;
+	}
+	
+	public boolean positionExists(Position position) {
+		return positionExists(position.getRow(), position.getColunm());
+	}
+	
+	public boolean thereIsAPiece(Position position) {
+		if (!positionExists(position)) {
+			throw new BoardException("Peća fora do tabuleiro");
+		}
+		return (piece(position) != null);
 	}
 	
 }
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -18,7 +18,8 @@ public class ChessMatch {
 	
 	public ChessPiece[][] getPieces(){
 		
-		ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColunms()];		
+		ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColunms()];	
+		
 		for(int i=0; i<board.getRows(); i++){
 			for (int j=0; j< board.getColunms(); j++) {
 				mat[i][j] = (ChessPiece) board.piece(i, j);
@@ -28,9 +29,10 @@ public class ChessMatch {
 	}
 	
 	private void initialSetup() {
-		board.placePiece(new Rook(board, Color.WHITE), new Position(2,1));
-		board.placePiece(new King(board, Color.BLACK), new Position(0,4));
-		board.placePiece(new King(board, Color.WHITE), new Position(7,4));
+		board.placePiece(new Rook(board, Color.WHITE), new Position(0,0));
+		board.placePiece(new Rook(board, Color.WHITE), new Position(0,7));
+		board.placePiece(new King(board, Color.WHITE), new Position(0,4));
+		board.placePiece(new King(board, Color.BLACK), new Position(7,4));
 	}
 	
 }
