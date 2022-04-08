b504c8cb26903c31c791c952beba28c1a0d8594e
KleberNevesTrindade <kleber.n.trindade@gmail.com>
2022-04-08 16:06:00 -0300

    Lista de peças capturadas

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,8 @@
 package application;
 
+import java.util.ArrayList;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 import chess.ChessException;
@@ -8,10 +10,10 @@ import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 
-//
+//--------------------------------------------
 // Estando em src/
 // java -cp /home/kleber/Documentos/WS-Eclipse/Curso/chess/bin application/Program
-//
+//--------------------------------------------
 
 public class Program {
 
@@ -19,12 +21,15 @@ public class Program {
 		
 		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
+		List<ChessPiece> captured = new ArrayList<>();
 		
+		//----------------------------------
+
 		while (true) {
 			try {
 				UI.clearScreen();
 				//UI.printBoard(chessMatch.getPieces());
-				UI.printMatch(chessMatch);
+				UI.printMatch(chessMatch, captured);
 				
 				System.out.println();
 				System.out.print("Source: ");
@@ -38,6 +43,9 @@ public class Program {
 				ChessPosition target = UI.readChessPosition(sc);
 
 				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+				if (capturedPiece != null){
+					captured.add(capturedPiece);
+				}
 
 			}
 			catch (ChessException e) {
